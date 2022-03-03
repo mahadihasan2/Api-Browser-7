2a47c0cc7b460209b92fa021366fac8832714e4d
mahadihasan2 <mahadihasan125643@gmail.com>
2022-03-04 00:05:19 +0600

    browser-Api-Start

---
+++ browser-api.js
@@ -0,0 +1,9 @@
+const number = 15;
+const student = {
+    name: 'Mahadi', friend: 'Masud', job: 'Seo',
+    seoservice: function (name) {
+        console.log(name)
+    }
+}
+student.seoservice('Doctor Website')
+// document.getElementById('name')
